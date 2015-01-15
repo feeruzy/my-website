b7a13393bcae67b32ecdfefd61e5b741d2b7c814
feeruzy <feeruzy@gmail.com>
2015-01-15 16:39:40 +0330

    first

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head lang="en">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Feeruzy's Home page</title>
+
+
+    <!--<link href="third-party/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <!--<link href="third-party/css/font-awesome.min.css" rel="stylesheet">-->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+
+    <script src="third-party/js/jquery.min.js"></script>
+    <script src="third-party/js/bootstrap.min.js"></script>
+    <script src="js/code.js"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+    <div id="intro" class="row">
+        <div class="nav">
+            <ul>
+                <li><a href="http://feeruzy.ir" >Home</a></li>
+                <li><a href="http://blog.feeruzy.ir" >Blog</a></li>
+                <li><a href="http://resume.feeruzy.ir" >Resume</a></li>
+            </ul>
+        </div>
+
+        <div id="title">
+            <p>Hi, I'm <strong>Feeruzy</strong>, <br> a web developer</p>
+        </div>
+    </div>
+
+    <div id="service" class="row">
+        <div class="content">
+
+            <h2>my services:</h2>
+
+            <div class="item">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-circle fa-stack-2x red"></i>
+                    <i class="fa fa-desktop fa-stack-1x fa-inverse"></i>
+                </span>
+                <p>Lorem ipsum eleifend bibendum proin tempor nunc mauris sapien, mauris per lacinia molestie ante fringilla adipiscing torquent, lobortis et orci augue cubilia erat hac et eleifend phasellus varius aptent suspendisse lacinia blandit ultrices pellentesque.
+                </p>
+            </div>
+
+            <div class="item">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-circle fa-stack-2x green"></i>
+                    <i class="fa fa-code fa-stack-1x fa-inverse"></i>
+                </span>
+                <p>Fames ultrices congue dolor pulvinar lobortis augue sollicitudin bibendum non facilisis morbi orci blandit cubilia et varius elit litora, vivamus fames sapien condimentum ullamcorper, primis commodo felis sollicitudin sagittis.</p>
+            </div>
+
+            <div class="item">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-circle fa-stack-2x orange"></i>
+                    <i class="fa fa-users fa-stack-1x fa-inverse"></i>
+                </span>
+                <p>Sodales egestas ligula ultrices vehicula dolor et libero cras nunc placerat rhoncus himenaeos, etiam sit consectetur non mauris lacus in mi dui blandit taciti est tincidunt nibh habitasse dolor fusce adipiscing taciti.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div id="info" class="row">
+
+        <div class="content">
+
+            <h2>Social Information:</h2>
+
+            <div class="item">
+                <span class="fa-2x fa-fw">
+                    <i class="fa fa-google-plus-square gmail"></i>
+                </span>
+                <p>Feeruzy@Gmail.com</p>
+            </div>
+
+            <div class="item">
+                <span class="fa-2x fa-fw">
+                    <i class="fa fa-phone-square phone"></i>
+                </span>
+                <p>+98-918-966-0541</p>
+            </div>
+
+            <div class="item">
+                <span class="fa-2x fa-fw">
+                    <i class="fa fa-linkedin-square linkedin"></i>
+                </span>
+                <p>Feeruzy</p>
+            </div>
+
+            <div class="item">
+                <span class="fa-2x fa-fw">
+                    <i class="fa fa-twitter-square twitter"></i>
+                </span>
+                <p>@_licence</p>
+            </div>
+
+            <div class="item">
+                <span class="fa-2x fa-fw">
+                    <i class="fa fa-github-square github"></i>
+                </span>
+                <p>Feeruzy</p>
+            </div>
+
+            <div class="item">
+                <span class="fa-2x fa-fw">
+                    <i class="fa fa-plus-square cv"></i>
+                </span>
+                <p>Resume</p>
+            </div>
+
+            <div class="item">
+                <span class="fa-2x fa-fw">
+                    <i class="fa fa-caret-square-o-right blog"></i>
+                </span>
+                <p>View my blog</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="footer">
+        &copy; 2013 - <?=date('Y')?>
+    </div>
+
+</div>
+
+
+
+</body>
+</html>
